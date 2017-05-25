99a3f540da83fe71de315b567c45dc5cf28b60ec
jaylenw <mr2cleanj@live.com>
2017-05-25 14:30:32 -0700

    fixed provision

--- NodeStaticSites/provision.sh
+++ NodeStaticSites/provision.sh
@@ -1,7 +1,10 @@
-sudo apt update  
-sudo apt install nodejs  
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y  
+sudo apt install nodejs -y  
 sudo ln -s /usr/bin/nodejs /usr/bin/node  
-sudo apt install npm
+sudo apt install npm -y
 sudo npm install -g npm
 sudo npm install -g bower
 sudo npm install -g grunt-cli
