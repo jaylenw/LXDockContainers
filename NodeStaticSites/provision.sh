61e7059a5f60a30633f45c1acb375c9b33148ec8
jaylenw <mr2cleanj@live.com>
2017-05-25 14:35:14 -0700

    added comments

--- NodeStaticSites/provision.sh
+++ NodeStaticSites/provision.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+echo "Upgrading System:"
 sudo apt update
 sudo apt upgrade -y  
+echo "Installing Node:"
 sudo apt install nodejs -y  
-sudo ln -s /usr/bin/nodejs /usr/bin/node  
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+echo "Installing NPM:"  
 sudo apt install npm -y
+echo "Upgrading NPM to latest version:"
 sudo npm install -g npm
+echo "Installing Bower:"
 sudo npm install -g bower
+echo "Installing Grunt:"
 sudo npm install -g grunt-cli
+echo "Installing Gulp:"
 sudo npm install -g gulp
