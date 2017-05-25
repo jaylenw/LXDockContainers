2184a5ddd56b98d977b109c93af872595e9486df
jaylenw <mr2cleanj@live.com>
2017-05-25 14:07:45 -0700

    completed provision.sh

---
+++ NodeStaticSites/provision.sh
@@ -0,0 +1,8 @@
+sudo apt update  
+sudo apt install nodejs  
+sudo ln -s /usr/bin/nodejs /usr/bin/node  
+sudo apt install npm
+sudo npm install -g npm
+sudo npm install -g bower
+sudo npm install -g grunt-cli
+sudo npm install -g gulp
