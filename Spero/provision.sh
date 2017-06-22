6957ae368c4b55ce1659cf62fa3260818b7cc768
jaylenw <mr2cleanj@live.com>
2017-06-21 23:15:16 -0700

    added commands for angularcli

--- Spero/provision.sh
+++ Spero/provision.sh
@@ -6,6 +6,13 @@ sudo apt upgrade -y
 sudo apt full-upgrade -y 
 echo "Installing Git:"
 sudo apt install git -y
+echo "Installing Nano:"
+sudo apt install nano -y
+echo "Installing SSH"
+sudo apt install ssh -y
+#
+# Spark Backend packages
+#
 echo "Installing OpenJDK 8 jre:"
 sudo apt install openjdk-8-jre -y
 echo "Installing OpenJDK 8 JDK"
@@ -14,11 +21,19 @@ echo "Installing MariaDB server:"
 sudo apt install mariadb-server -y
 echo "Installing MariaDB client:"
 sudo apt install mariadb-client -y
-echo "Installing Nano:"
-sudo apt install nano -y
 echo "Installing Maven:"
 sudo apt install maven -y
-echo "Installing Chromium"
+echo "Installing Chromium:"
 sudo apt install chromium-browser -y
-echo "Installing SSH"
-sudo apt install ssh -y
+#
+# Angular Specific Packages
+#
+echo "Installing nodejs:"
+sudo apt install nodejs -y
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+echo "Installing and Upgrading NPM:"
+sudo apt install npm
+sudo npm install -g npm
+echo "Installing Angular CLI:"
+sudo npm install -g @angular/cli
+
