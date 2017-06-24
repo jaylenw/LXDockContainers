62c04d87f1f8d71cac3d756afe847ec315e421eb
jaylenw <mr2cleanj@live.com>
2017-06-24 02:50:13 -0700

    added more packages for unit testing

--- Spero/provision2.sh
+++ Spero/provision2.sh
@@ -8,9 +8,14 @@ echo "Installing Git:"
 sudo apt install git -y
 echo "Installing Curl:"
 sudo apt install curl -y
-echo "Installing Python"
+echo "Installing Python:"
 sudo apt install python -y
-echo "Installing Node and NPM"
+echo "Installing Gdebi:"
+sudo apt install gdebi -y
+echo "Installing Google Chrome:"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo gdebi google-chrome-stable_current_amd64.deb --n
+echo "Installing Node and NPM:"
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt install nodejs -y
 sudo ln -s /usr/bin/nodejs /usr/bin/node
