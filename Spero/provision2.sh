39810d786a250bed61e9aa298619b647266c0f06
jaylenw <mr2cleanj@live.com>
2017-06-23 21:32:16 -0700

    added node and angulare installation

---
+++ Spero/provision2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Upgrading System:"
+sudo apt update
+sudo apt upgrade -y 
+sudo apt full-upgrade -y 
+echo "Installing Git:"
+sudo apt install git -y
+echo "Installing Curl:"
+sudo apt install curl -y
+echo "Installing Python"
+sudo apt install python -y
+echo "Installing Node and NPM"
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt install nodejs -y
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+echo "Are you a root user? Type 'y' or 'n':"
+read answer
+if[["$answer" == "y"]]
+then npm config set unsafe-perm=true
+else echo "NPM root config not set"
+fi
+sudo npm install npm -g #if root user, run npm config set unsafe-perm=true
+echo "Installing AngularCLI"
+sudo npm install -g @angular/cli
